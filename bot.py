8a58ca8183252b24923734c0869519c0acbc9bc6
alaansamuel6-glitch <alaansamuel6@gmail.com>
2026-06-25 21:01:24 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
-import telebot # библиотека telebot
-from config import token # импорт токена
+import telebot
+from config import token
 
-bot = telebot.TeleBot(token) 
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -9,18 +9,41 @@ def start(message):
 
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
-    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
-        chat_id = message.chat.id # сохранение id чата
-         # сохранение id и статуса пользователя, отправившего сообщение
+    if message.reply_to_message:
+        chat_id = message.chat.id
         user_id = message.reply_to_message.from_user.id
-        user_status = bot.get_chat_member(chat_id, user_id).status 
-         # проверка пользователя
+        user_status = bot.get_chat_member(chat_id, user_id).status
         if user_status == 'administrator' or user_status == 'creator':
             bot.reply_to(message, "Невозможно забанить администратора.")
         else:
-            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.ban_chat_member(chat_id, user_id)
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
-        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+        bot.reply_to(message, "Ответьте на сообщение пользователя командой /ban")
+
+@bot.message_handler(commands=['unban'])
+def unban_user(message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        user_id = message.reply_to_message.from_user.id
+        bot.unban_chat_member(chat_id, user_id)
+        bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был разбанен.")
+    else:
+        bot.reply_to(message, "Ответьте на сообщение пользователя командой /unban")  # ТЕПЕРЬ ОТВЕЧАЕТ!
+
+@bot.message_handler(func=lambda message: True)
+def ban_for_links(message):
+    if message.text and "https://" in message.text:
+        chat_id = message.chat.id
+        user_id = message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status
+        if user_status != 'administrator' and user_status != 'creator':
+            bot.ban_chat_member(chat_id, user_id)
+            bot.reply_to(message, f"Пользователь @{message.from_user.username} был забанен за отправку ссылки.")
+
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)      
 
 bot.infinity_polling(none_stop=True)
